85e761e179dac4c01cc7b74ab30339035aba5516
sophiadejong <sophiamarinadejongy@gmail.com>
2017-05-07 05:59:03 +0200

    Add files via upload

---
+++ js/volume-setup.js
@@ -0,0 +1,44 @@
+var audioContext = null;
+var meter = null;
+var rafID = null;
+var mediaStreamSource = null;
+
+window.onload = function() {
+
+    // monkeypatch Web Audio
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	
+    // grab an audio context
+    audioContext = new AudioContext();
+
+    // Attempt to get audio input
+    try {
+        // monkeypatch getUserMedia
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+
+        // ask for an audio input
+        navigator.getUserMedia(
+        {
+            "audio": {
+                "mandatory": {
+                    "googEchoCancellation": "false",
+                    "googAutoGainControl": "false",
+                    "googNoiseSuppression": "false",
+                    "googHighpassFilter": "false"
+                },
+                "optional": []
+            },
+        }, gotStream, didntGetStream);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+
+}
+
+function didntGetStream() {
+    alert('Stream generation failed.');
+}
+
